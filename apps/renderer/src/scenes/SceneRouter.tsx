66f935341b342a0df1c85ca2885efc74d19b51cc
Remco Land <remcoland59@gmail.com>
2026-09-16 17:29:25 +0200

    feat(renderer): route versatile scene types

--- apps/renderer/src/scenes/SceneRouter.tsx
+++ apps/renderer/src/scenes/SceneRouter.tsx
@@ -4,6 +4,7 @@ import {SafeComparisonScene} from "./SafeComparisonScene";
 import {SafeFlowchartScene} from "./SafeFlowchartScene";
 import {SceneRenderer} from "./SceneRenderer";
 import {StructuredSceneRenderer} from "./StructuredScenes";
+import {VersatileSceneRenderer} from "./VersatileScenes";
 
 type Project=NormalizedStudyTubeProject["project"];
 
@@ -13,6 +14,7 @@ export const SceneRouter=({normalizedScene,project}:{normalizedScene:NormalizedS
     case "flowchart":return <SafeFlowchartScene scene={normalizedScene.scene}/>;
     case "timeline":case "process":case "diagram":case "iconScene":return <StructuredSceneRenderer normalizedScene={normalizedScene}/>;
     case "image":case "document":case "documentHighlight":case "visualGag":return <MediaSceneRenderer normalizedScene={normalizedScene} project={project}/>;
+    case "bulletReveal":case "annotatedImage":case "dataChart":case "matrix":case "cycle":case "multipleChoice":case "workedExample":case "hierarchy":return <VersatileSceneRenderer normalizedScene={normalizedScene} project={project}/>;
     default:return <SceneRenderer normalizedScene={normalizedScene}/>;
   }
 };
