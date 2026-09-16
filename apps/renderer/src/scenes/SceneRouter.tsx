a97aa95f8a8fedd8bc2e6b1a7634930e54972e95
Remco Land <remcoland59@gmail.com>
2026-09-16 21:34:42 +0200

    Route annotated images through aligned renderer

--- apps/renderer/src/scenes/SceneRouter.tsx
+++ apps/renderer/src/scenes/SceneRouter.tsx
@@ -1,4 +1,5 @@
 import type {NormalizedScene,NormalizedStudyTubeProject} from "@studytube/core";
+import {AnnotatedImageScene} from "./AnnotatedImageScene";
 import {MediaSceneRenderer} from "./MediaScenes";
 import {QuoteScene} from "./QuoteScene";
 import {SafeComparisonScene} from "./SafeComparisonScene";
@@ -14,9 +15,10 @@ export const SceneRouter=({normalizedScene,project}:{normalizedScene:NormalizedS
     case "comparison":return <SafeComparisonScene scene={normalizedScene.scene}/>;
     case "flowchart":return <SafeFlowchartScene scene={normalizedScene.scene}/>;
     case "quote":return <QuoteScene scene={normalizedScene.scene}/>;
+    case "annotatedImage":return <AnnotatedImageScene scene={normalizedScene.scene} project={project}/>;
     case "timeline":case "process":case "diagram":case "iconScene":return <StructuredSceneRenderer normalizedScene={normalizedScene}/>;
     case "image":case "video":case "document":case "documentHighlight":case "visualGag":return <MediaSceneRenderer normalizedScene={normalizedScene} project={project}/>;
-    case "bulletReveal":case "annotatedImage":case "dataChart":case "matrix":case "cycle":case "multipleChoice":case "workedExample":case "hierarchy":return <VersatileSceneRenderer normalizedScene={normalizedScene} project={project}/>;
+    case "bulletReveal":case "dataChart":case "matrix":case "cycle":case "multipleChoice":case "workedExample":case "hierarchy":return <VersatileSceneRenderer normalizedScene={normalizedScene} project={project}/>;
     default:return <SceneRenderer normalizedScene={normalizedScene}/>;
   }
 };
